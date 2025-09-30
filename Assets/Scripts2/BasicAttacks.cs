d154bed48db4254e2ffe9387ca2870e3d1a0a393
Predrag777 <predragbrajovicSS@gmail.com>
2025-09-30 21:46:05 +0200

    Introduce particle system for power punch

--- Assets/Scripts2/BasicAttacks.cs
+++ Assets/Scripts2/BasicAttacks.cs
@@ -2,12 +2,14 @@ using UnityEngine;
 using System.Collections;
 public class BasicAttacks : MonoBehaviour
 {
+    [SerializeField] private ParticleSystem powerUp;
     private float damage = 5f;
     private float speed = 1f;
     private Animator animator;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        powerUp.Stop();
         animator = GetComponent<Animator>();
     }
 
@@ -23,6 +25,13 @@ public class BasicAttacks : MonoBehaviour
     private IEnumerator PlayAttacks()
     {
         animator.Play("punch");
+        powerUp.Play();
+        for (int i = 0; i < 3; i++)
+        {
+            transform.position += transform.forward * 30f * Time.fixedDeltaTime;
+            yield return new WaitForSeconds(0.1f);
+
+        }
         yield return new WaitForSeconds(speed);
     }
 }
